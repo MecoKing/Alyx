04c029f6365ad783d280a08f4a678dee14f71f11
Micah Merswolke <mecoking@icloud.com>
2015-05-09 08:46:15 -0400

    Added a list of vocabulary to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Alyx { //Protector of Humanity
 	class Program {
+
+		public static List<Word> vocab = new List<Word> ();
+
 		public static void Main (string[] args) {
-			
 			for (;;) {
 				string input = Console.ReadLine ();
 			}
