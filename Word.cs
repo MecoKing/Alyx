a633fde592953599a865a5a6b15460785944be4b
Micah Merswolke <mecoking@icloud.com>
2015-05-09 08:46:34 -0400

    Made a tag searching method to gather lists of similar words

--- Word.cs
+++ Word.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Alyx {
 	public class Word {
@@ -7,7 +8,8 @@ namespace Alyx {
 		string tags;
 		string phonetic;
 
-		//A script will look like this: [name: tag anotherTag thirdTag moarTags]
+		//A script will look like this:
+		//    name: tag anotherTag thirdTag moarTags
 		public Word (string script) {
 			string subScript = "";
 			for (int i = 0; i < script.Length; i++) {
@@ -23,6 +25,28 @@ namespace Alyx {
 			}
 			phonetic = Phonetic.generatePhoneticsFor (name);
 		}
+
+		// |tag = OR includes tag
+		// &tag = AND includes tag
+		// wordsTaggedFromCollection (Program.vocab, "noun", "&mammal", "&animal");
+		public static Word[] wordsTaggedFromCollection (Word[] collection, params string[] searchTags) {
+			List<Word> taggedWords = new List<Word> ();
+			foreach (string tag in searchTags) {
+				foreach (Word term in collection) {
+					if (tag.StartsWith ("|") || !tag.StartsWith ("&")) {
+						if (term.tags.Contains (tag.Remove (0, 1)))
+							taggedWords.Add (term);
+					} else if (tag.StartsWith ("&")) {
+						if (taggedWords.Contains (term)) {
+							if (!term.tags.Contains (tag.Remove (0, 1)))
+								taggedWords.Remove (term);
+						}
+					}
+				}
+			}
+			return taggedWords.ToArray ();
+		}
+
 	}
 }
 
