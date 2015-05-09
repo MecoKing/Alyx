a427245c66e637caae33038679f4cca1ec59eba8
Micah Merswolke <mecoking@icloud.com>
2015-05-09 07:52:39 -0400

    Made words generate based on a formatted script
    script = “name: tag1 tag2 tag3”

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 namespace Alyx { //Protector of Humanity
 	class Program {
 		public static void Main (string[] args) {
+			
 			for (;;) {
 				string input = Console.ReadLine ();
 			}
--- Word.cs
+++ Word.cs
@@ -7,7 +7,21 @@ namespace Alyx {
 		string tags;
 		string phonetic;
 
+		//A script will look like this: [name: tag anotherTag thirdTag moarTags]
 		public Word (string script) {
+			string subScript = "";
+			for (int i = 0; i < script.Length; i++) {
+				if (script [i] == ':') {
+					name = subScript;
+					subScript = "";
+				} else if (i == script.Length - 1) {
+					subScript += script [i];
+					tags = subScript;
+					subScript = "";
+				} else
+					subScript += script [i];
+			}
+			phonetic = Phonetic.generatePhoneticsFor (name);
 		}
 	}
 }
