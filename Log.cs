2a58a725dd47a901b3412ac74f1cad698a8951c0
Micah Merswolke <mecoking@icloud.com>
2015-05-11 19:57:24 -0400

    Added a log class for logging within code
    There is probably already something like this in C# but I haven’t
    bothered checking.

---
+++ Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Alyx {
+	public class Log {
+
+		public static void Write (string file, string tag, string message) {
+			using (StreamWriter writer = new StreamWriter ("Debug.txt", true)) {
+				writer.WriteLine ("[{0}] {1}: {2}", file, tag, message);
+			}
+		}
+
+	}
+}
+
