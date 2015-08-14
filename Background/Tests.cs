a37621c4916853f41e6ff6862082ada653b41868
Micah Merswolke <mecoking@icloud.com>
2015-08-14 14:00:05 -0400

    Moved Log.cs and Tests.cs to a separate folder
    Here they can be friends and hang out in their very exclusive
    ‘background’ club. Any other files that are used for background under
    the hood work are welcome to join the club. :D

--- Background/Tests.cs
+++ Background/Tests.cs
@@ -13,10 +13,10 @@ namespace Alyx {
 				"week", "month", "year", "generic", "abstract", "specific", "language", "art", "music", "math", "science", "religion", "politics", "commerce", "male", "female",
 				"true", "false", "positive", "negative", "builder", "warrior", "healer", "bright", "dim", "dark", "noun", "adverb", "adjective", "Verb", "pronoun", "article",
 				"conjunction", "name", "preposition", "front", "right", "back", "left", "in", "out", "above", "below", "plural", "collective", "question", "exclamation",
-				"self", "possession", "happy", "sad", "love", "anger", "bored", "municipal", "continental", "planetary", "galactic", "universal", "sense", "see", "hear", "feel",
-				"smell", "taste", "tiny", "small", "medium", "big", "large", "huge", "animal", "plant", "human", "alien", "living", "dead", "residential", "commercial", "industrial",
-				"entertainment", "business", "nature", "governmental", "cold", "temperate", "warm", "hot", "past", "present", "start", "end", "future", "emotional", "physical",
-				"digital", "virtual", "material", "spiritual", "social", "mental", "cultural",
+				"self", "possession", "command", "happy", "sad", "love", "anger", "bored", "municipal", "continental", "planetary", "galactic", "universal", "sense", "see", "hear",
+				"feel", "smell", "taste", "tiny", "small", "medium", "big", "large", "huge", "animal", "plant", "human", "alien", "living", "dead", "residential", "commercial",
+				"industrial", "entertainment", "business", "nature", "governmental", "cold", "temperate", "warm", "hot", "past", "present", "start", "end", "future", "emotional",
+				"physical", "digital", "virtual", "material", "spiritual", "social", "mental", "cultural",
 
 				"italy",
 			};
