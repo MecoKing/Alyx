da5ee31855fdc8f5810d011a23e3620332664ce3
Micah Merswolke <mecoking@icloud.com>
2015-08-17 11:17:31 -0400

    Added commands
    Alyx now dissects sentences containing commands and decides what the
    user wants done. Then does it. (Current commands: Show/Hide Analysis)

---
+++ Command.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alyx {
+	public class Command {
+
+		static string orders = "Show Hide";
+		static string modifiers = "Analysis";
+		static Dictionary<string, string> orderMods = new Dictionary<string, string> () {
+			{"Show", "Analysis"},
+			{"Hide", "Analysis"}
+		};
+
+		static string command;
+
+		/// <summary> Checks for a command in the given sentence. Runs the command if found. </summary>
+		public static void checkForOrderIn (Sentence phrase) {
+			List<string> possibleOrder = new List<string> ();
+			List<string> possibleModifier = new List<string> ();
+
+			foreach (Word term in phrase.words) {
+				if (orders.Contains (term.name))
+					possibleOrder.Add (term.name);
+				else if (modifiers.Contains (term.name))
+					possibleModifier.Add (term.name);
+			}
+
+			foreach (string order in possibleOrder) {
+				foreach (string mod in possibleModifier) {
+					if (orderMods [order].Contains (mod))
+						//What are the odds that two commands would appear in one sentence?
+						command = string.Format ("{0} {1}", order, mod);
+				}
+			}
+			if (command != null) {
+				if (Program.showAnalysis)
+					Console.WriteLine ("COMMAND: {0}", command);
+				runCommand ();
+			}
+		}
+
+		static void runCommand () {
+			//Try to keep these in alphabetical order
+			if (command == "Hide Analysis")
+				Program.showAnalysis = false;
+			else if (command == "Show Analysis")
+				Program.showAnalysis = true;
+		}
+	}
+}
+
